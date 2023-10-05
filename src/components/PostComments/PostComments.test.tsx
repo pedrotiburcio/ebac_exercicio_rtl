6d7cdec4af80869150796738ceb63d057faa2508
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2023-10-05 15:14:27 -0300

    testando a inserção de comentários com jest e testing-library

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+    
+    test('Deve adicionar dois comentários corretamente', () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Primeiro comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Segundo comentário'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+    
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument()
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument()
     });
 });
